a2b933df994884880f4a4258d745b283ffd43dce
vishubth <59112242+vishubth@users.noreply.github.com>
2026-06-04 14:03:54 +0530

    Fix import statement for useAuth in ProtectedRoute

--- src/components/auth/ProtectedRoute.jsx
+++ src/components/auth/ProtectedRoute.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { Navigate } from "react-router-dom";
 
+import { useAuth } from "../../context/AuthContext";
+
 export default function ProtectedRoute({
   children,
   requireAdmin = false,
 }) {
-  import { useAuth } from "../../context/AuthContext";
   const { auth } = useAuth();
 
   if (!auth?.token) {
